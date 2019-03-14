8508fd52d47310036475a4d62a7ba1f79d024a4f
Felipe Silveira <eng.fe.silveira@gmail.com>
2019-03-13 20:36:28 -0400

    finalizando uma custom manager do models

--- shell-models-3.py
+++ shell-models-3.py
@@ -6,3 +6,12 @@ import django
 django.setup()
 
 from courses.models import Course 
+
+print(type(Course.objects))
+print(type(Course.objects.all()))
+
+Course.objects.create(name='Python mais uma vez', slug='django')
+Course.objects.create(name='VisualStudio', slug='python')
+Course.objects.create(name='Algoritmos', slug='portunhol')
+
+print(Course.objects.search('python'))
